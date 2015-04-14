1627d80c10900fb8a9d845747ee142413ed62587
Andy Pickler <andy.pickler@gmail.com>
2015-04-14 15:25:47 -0600

    Additions from solution's test-helper file fixed test flakiness

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -4,3 +4,13 @@ import {
 } from 'ember-qunit';
 
 setResolver(resolver);
+
+document.write('<div id="ember-testing-container"><div id="ember-testing"></div></div>');
+
+QUnit.config.testTimeout = 4000;
+
+QUnit.config.reorder = false;
+
+QUnit.config.urlConfig.push({ id: 'nocontainer', label: 'Hide container'});
+var containerVisibility = QUnit.urlParams.nocontainer ? 'hidden' : 'visible';
+document.getElementById('ember-testing-container').style.visibility = containerVisibility;
